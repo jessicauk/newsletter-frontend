15bedf3664c580ba2245da7349ac4e38347dc666
Jessica Apolinar <jessica_220691@hotmail.com>
2024-03-11 21:15:57 -0600

    -a

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,6 @@ export default function Page() {
     <AppContext>
       <Layout>
         <Compose />
-        {/* <div className="box-border bg-slate-200 p-8 rounded-xl shadow-2xl col-span-full w-3/4 sm:w-2/4 lg:w-2/4 xl:w-2/4 h-5/6 flex flex-col justify-evenly content-center items-center">
-          <Form />
-        </div> */}
       </Layout>
     </AppContext>
   );
